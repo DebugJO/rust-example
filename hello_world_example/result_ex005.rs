b706d0e7d8a16963bfc88ad66089a6236c02e2ab
msjo <me@msjo.kr>
2022-02-18 23:13:47 +0900

    Update result_ex005.rs

--- hello_world_example/result_ex005.rs
+++ hello_world_example/result_ex005.rs
@@ -9,3 +9,26 @@ fn main() {
         }
     }
 }
+
+// fn main() {
+//     let item_vec = vec![vec!["홍길동", "10"], vec!["가나닭", "20", "30"]];
+
+//     let mut result:Vec<i32> = Vec::new();
+//     let mut result_error:Vec<String> = Vec::new();
+
+//     for mut item in item_vec {
+//         while let Some(info) = item.pop() {
+//             if let Ok(number) = info.parse::<i32>() {
+//                 result.push(number);
+//                 println!("The number is: {}", number);
+//             } else if let Err(e) = info.parse::<i32>() {
+//                 result_error.push(e.to_string());
+//                 println!("Error : {}", e);
+//             }
+//         }
+//     }
+
+//     result.sort();
+//     println!("{:?} : {}", result, result.len());
+//     println!("{:?} : {}", result_error, result_error.len());
+// }
